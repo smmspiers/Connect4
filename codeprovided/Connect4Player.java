738802759935098347af40ad969bb44dc5942473
smmspiers <sammyspiers@yahoo.co.uk>
2017-03-02 15:20:02 +0000

    Still initialising

---
+++ codeprovided/Connect4Player.java
@@ -0,0 +1,14 @@
+package assignment2017.codeprovided;
+
+import assignment2017.codeprovided.Connect4GameState;
+
+public abstract class Connect4Player {
+
+	/**
+	 * Decides which column in which to move, and then calls move on the gameState instance
+	 * in order to make the move.  The method is free to query gameState (i.e. through the
+	 * getCounterAt method in order to decide which move to make)
+	 * @param gameState the current Connect4 game state
+	 */
+	public abstract void makeMove(Connect4GameState gameState);
+}
