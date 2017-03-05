9238c28608e4e6a415b5414b7b4fbeaf27ace049
smmspiers <sammyspiers@yahoo.co.uk>
2017-03-05 17:31:02 +0000

    Start Extended abstract classes and implement interfaces from code given

---
+++ MyGameState.java
@@ -0,0 +1,42 @@
+package assignment2017;
+
+import assignment2017.codeprovided.Connect4GameState;
+
+public class MyGameState extends Connect4GameState {
+
+    public void startGame() {
+
+    }
+
+    public int getCounterAt(int col, int row) {
+        return;
+    }
+
+    public int whoseTurn() {
+        return;
+    }
+
+    public boolean gameOver() {
+        return;
+    }
+
+    public boolean isBoardFull() {
+        return;
+    }
+
+    public int getWinner() {
+
+    }
+
+    public void move(int col) {
+
+    }
+
+    public boolean isColumnFull(int col) {
+        return;
+    }
+
+    public Connect4GameState copy() {
+        return;
+    }
+}
